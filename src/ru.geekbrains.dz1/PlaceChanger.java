8e46580ef124a6d5a5ca4572ec6fcb53c01ddac9
AnastasiaDro <adrogunova@gmail.com>
2018-09-15 22:00:18 +0300

    -Добавлен класс PlaceChanger для упражнения №1: менять местами элементы массива любого ссылочного типа -написан конструктор метода, принимающий на вход массив любого типа

---
+++ src/ru.geekbrains.dz1/PlaceChanger.java
@@ -0,0 +1,14 @@
+package ru.geekbrains.dz1;
+
+import java.util.ArrayList;
+
+public class PlaceChanger <T> {
+    T[] arr;
+
+    public PlaceChanger (T[] arr) {
+        this.arr = arr;
+    }
+
+
+
+}
