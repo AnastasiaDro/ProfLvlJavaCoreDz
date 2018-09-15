c05829dcf48066ae7816fe8022f5738c74245bc3
AnastasiaDro <adrogunova@gmail.com>
2018-09-16 00:46:35 +0300

    В класс Fruit добавлен метод getFruitWeight() - возвращает вес фрукта

--- src/ru/geekbrains/dz1/part2fruitTask/Fruit.java
+++ src/ru/geekbrains/dz1/part2fruitTask/Fruit.java
@@ -1,10 +1,15 @@
 package ru.geekbrains.dz1.part2fruitTask;
 
 public class Fruit {
-    float weight;
+    private float weight;
+
 
     public Fruit (float weight) {
         this.weight = weight;
+    }
+
+    public float getFruitWeight() {
+        return weight;
     }
 
 }
