16b9940f96e4e1ff9dae18721700a6182a885a24
AnastasiaDro <adrogunova@gmail.com>
2018-09-15 22:51:54 +0300

    Изменен метод arrToArrayList: использование Arrays.asList() вместо Collections.addAll()

--- src/ru.geekbrains.dz1/PlaceChanger.java
+++ src/ru.geekbrains.dz1/PlaceChanger.java
@@ -1,23 +1,35 @@
 package ru.geekbrains.dz1;
 
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 public class PlaceChanger <T> {
     private T[] arr;
     //переменная для записи элемента
     private T container;
+    private ArrayList newArrayList;
 
-    public PlaceChanger (T[] arr) {
-        this.arr = arr;
+    public PlaceChanger () {
+
     }
 
-    //сам метод смены мест ПО ИНДЕКСУ
-    public T[] placeChangerByIndex (int firstIndex, int secondIndex) {
+    // Задание 1
+    // сам метод смены мест ПО ИНДЕКСУ
+    public T[] placeChangerByIndex (T[] arr, int firstIndex, int secondIndex) {
         container = arr[firstIndex];
         arr[firstIndex] = arr[secondIndex];
         arr[secondIndex] = container;
         return arr;
     }
 
+    // Задание 2
+    // метод преобразования массива в ArrayList
+    public ArrayList arrToArrayList(T[] arr) {
+        newArrayList.add(Arrays.asList(arr));
+        return newArrayList;
+    }
 
 
 }
