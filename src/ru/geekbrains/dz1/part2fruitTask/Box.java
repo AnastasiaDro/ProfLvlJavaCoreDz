52a54a168ba60be84942691a6ac3388b406887a1
AnastasiaDro <adrogunova@gmail.com>
2018-09-16 01:08:17 +0300

    В класс Box добавлены:
    переменные:
    - boxArrayList - для хранения фруктов в коробке
    - boxWeight - вес коробки
    
    метод:
    - putFruit(T fruit) - добавляет фрукт в коробку

--- src/ru/geekbrains/dz1/part2fruitTask/Box.java
+++ src/ru/geekbrains/dz1/part2fruitTask/Box.java
@@ -1,5 +1,17 @@
 package ru.geekbrains.dz1.part2fruitTask;
 
+
+import java.util.ArrayList;
+
 public class Box <T extends Fruit> {
+    ArrayList<T> boxArrayList = new ArrayList<>();
+    Float boxWeight;
+
+
+    public void putFruit(T fruit) {
+        boxArrayList.add(fruit);
+    }
+
+
 
 }
