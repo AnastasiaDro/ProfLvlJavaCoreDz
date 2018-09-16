6324cca4aa0e2d84ad3ce2bbbdaf4ac6466941a3
AnastasiaDro <adrogunova@gmail.com>
2018-09-16 20:47:55 +0300

    В классе Box:
    добавлен метод:
    - getWeight() - узнать вес коробки
    
    изменён метод:
    -putFruit(T fruit) - вес коробки изменяется сразу при добавлении туда фрукта

--- src/ru/geekbrains/dz1/part2fruitTask/Box.java
+++ src/ru/geekbrains/dz1/part2fruitTask/Box.java
@@ -8,9 +8,18 @@ public class Box <T extends Fruit> {
     Float boxWeight;
 
 
+//положить фрукт в коробку (Сразу изменим её вес)
     public void putFruit(T fruit) {
         boxArrayList.add(fruit);
+        boxWeight+=fruit.getFruitWeight();
     }
+
+//узнать вес коробки
+    public float getWeight() {
+        return boxWeight;
+    }
+
+
 
 
 
