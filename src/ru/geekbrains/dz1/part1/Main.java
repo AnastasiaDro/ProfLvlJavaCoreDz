b4a0d38aaa80f361e6ff51b3135451cd96bed3c2
AnastasiaDro <adrogunova@gmail.com>
2018-09-24 08:41:17 +0300

    Выполнен Soft-reset ветки мастер, чтобы сдать домашку

---
+++ src/ru/geekbrains/dz1/part1/Main.java
@@ -0,0 +1,10 @@
+package ru.geekbrains.dz1.part1;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
---
+++ src/ru/geekbrains/dz1/part2fruitTask/FruitMain.java
@@ -0,0 +1,9 @@
+package ru.geekbrains.dz1.part2fruitTask;
+
+
+
+public class FruitMain {
+    public static void main(String[] args)  {
+
+    }
+}
