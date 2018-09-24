f58b22062b69fa7d746a5aa1d1afab6e0a643607
AnastasiaDro <adrogunova@gmail.com>
2018-09-24 09:27:11 +0300

    Добавлены классы: - Fruit, и его наследники: - Apple - Orange

---
+++ src/ru/geekbrains/dz1/part2fruitTask/Apple.java
@@ -0,0 +1,10 @@
+package ru.geekbrains.dz1.part2fruitTask;
+
+
+public class Apple extends Fruit {
+
+    public Apple() {
+        weight = 1.0f;
+    }
+
+}
---
+++ src/ru/geekbrains/dz1/part2fruitTask/Fruit.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.dz1.part2fruitTask;
+
+public class Fruit {
+    float weight;
+
+    public Fruit() {
+
+    }
+    public float getFruitWeight() {
+        return weight;
+    }
+
+}
