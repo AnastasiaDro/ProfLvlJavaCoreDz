674a2605ffe4fe3aea97235fdf11b64861da4394
AnastasiaDro <adrogunova@gmail.com>
2018-09-26 22:33:06 +0300

    Попытка подключить FX  к Maven проекту

--- src/ru/geekbrains/dz2/client/ClientMain.java
+++ src/ru/geekbrains/dz2/client/ClientMain.java
@@ -1,8 +1,24 @@
 package ru.geekbrains.dz2.client;
 
-public class ClientMain {
-    public static void main(String[] args) {
 
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import static javafx.fxml.FXMLLoader.load;
+
+public class ClientMain extends Application {
+
+
+
+
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle( "JavaChat" );
+        primaryStage.setScene( new Scene(root, 400, 400 ));
+        primaryStage.show();
     }
 
+    public static void main(String[] args) { launch(args); }
 }
--- src/ru/geekbrains/dz2/client/Controller.java
+++ src/ru/geekbrains/dz2/client/Controller.java
@@ -1,4 +1,30 @@
 package ru.geekbrains.dz2.client;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+
 public class Controller {
+    @FXML
+    TextArea textArea;
+    @FXML
+    TextField msgField;
+    @FXML
+    HBox msgPanel;
+    @FXML
+    HBox authPanel;
+    @FXML
+    TextField loginField;
+    @FXML
+    PasswordField passField;
+
+
+    public void sendAuthMsg(ActionEvent actionEvent) {
+    }
+
+    public void sendMsg(ActionEvent actionEvent) {
+    }
 }
