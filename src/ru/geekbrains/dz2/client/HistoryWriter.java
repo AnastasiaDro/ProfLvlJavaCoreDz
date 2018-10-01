f4ba52e1a008c595f382b41f94877fd1d8fa7ae0
AnastasiaDro <adrogunova@gmail.com>
2018-10-01 10:02:57 +0300

    Обновлён класс HistoryWriter Добавлен метод saveHistory();

--- src/ru/geekbrains/dz2/client/HistoryWriter.java
+++ src/ru/geekbrains/dz2/client/HistoryWriter.java
@@ -1,4 +1,22 @@
 package ru.geekbrains.dz2.client;
 
+import java.awt.*;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class HistoryWriter {
+    BufferedWriter bw;
+
+
+    public void saveHistory(TextArea textArea) {
+        try {
+            bw = new BufferedWriter(new FileWriter( "src\\ru\\geekbrains\\dz2\\client\\history.txt" ) );
+            bw.write( textArea.getText() );
+            bw.close();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
 }
