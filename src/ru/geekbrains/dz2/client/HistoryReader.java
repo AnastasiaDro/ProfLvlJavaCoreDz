c66d9a23ae79b780eb68989bbc96665577460a7b
AnastasiaDro <adrogunova@gmail.com>
2018-10-01 13:18:11 +0300

    Изменен метод HistoryReader: теперь в чат добавляются только последние 100 строк из файла history - сделано с помощью Array List и потому кажется не очень красивым
    Проблема: если подключено несколько пользователей, то изменения в чате дублируются, так как в двух потоках подключены к одному и тому же файлу.
    НО: если пользователи будут подключены с разных машин,то такого быть не должно.
    Вариант 2: писать историю в базу данных

--- src/ru/geekbrains/dz2/client/HistoryReader.java
+++ src/ru/geekbrains/dz2/client/HistoryReader.java
@@ -1,24 +1,39 @@
 package ru.geekbrains.dz2.client;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class HistoryReader {
     BufferedReader br = null;
-
+    ArrayList <String> myArr = new ArrayList(  );
 
     public String readHistory() throws FileNotFoundException {
         String answer = "";
+        int minus = 0;
+
         try {
         br = new BufferedReader( new FileReader( "src\\ru\\geekbrains\\dz2\\client\\history.txt") );
         String str;
 
         //условие на 100 строк
 
-
-
-
         while((str = br.readLine()) != null)
-            answer += str+"\n";
+            myArr.add( str+"\n" );
+        if (myArr.size()>100) {
+           minus = myArr.size()-100;
+            for (int i = 0; i < minus; i++) {
+                myArr.remove( 0 );
+            }
+        }
+            StringBuilder builder = new StringBuilder();
+            for (String value : myArr) {
+                builder.append(value);
+            }
+
+
+            answer += builder.toString();
+
             answer+="Текущая переписка: \n";
             br.close();
         } catch (IOException e) {
@@ -29,6 +44,29 @@ public class HistoryReader {
         }
 
     }
+
+//    public String readHistory() throws FileNotFoundException {
+//        String answer = "";
+//        try {
+//        br = new BufferedReader( new FileReader( "src\\ru\\geekbrains\\dz2\\client\\history.txt") );
+//        String str;
+//
+//        //условие на 100 строк
+//
+//
+//
+//        while((str = br.readLine()) != null)
+//            answer += str+"\n";
+//            answer+="Текущая переписка: \n";
+//            br.close();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//            answer = "История пуста";
+//        } finally {
+//            return answer;
+//        }
+//
+//    }
 
 
 }
