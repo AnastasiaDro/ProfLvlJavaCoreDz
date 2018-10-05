976c97b2f680c619ea8eed2a8e0deab1140a058b
AnastasiaDro <adrogunova@gmail.com>
2018-10-05 15:11:03 +0300

    Снова
    -изменен MyLetterThread

--- src/ru/geekbrains/dz4/MyLetterThread.java
+++ src/ru/geekbrains/dz4/MyLetterThread.java
@@ -2,30 +2,65 @@ package ru.geekbrains.dz4;
 
 import org.omg.PortableServer.THREAD_POLICY_ID;
 
+import java.util.List;
+
 public class MyLetterThread implements Runnable {
     char letter;
     int exitNumber;
+    //число потоков
+    int maxNumber = 3;
+    //счётчик номера потока
+    int counter = 1;
     Thread t;
-//конструктор
-public MyLetterThread (char letter, int exitNumber) {
-    this.letter  = letter;
-    this.exitNumber = exitNumber;
-    this.t = new Thread(this  );
-    t.start();
-}
 
+    //конструктор
+    public MyLetterThread(char letter, int exitNumber) {
+        this.letter = letter;
+        this.exitNumber = exitNumber;
+        this.t = new Thread( this );
+        t.start();
+    }
 
 
     @Override
     public void run() {
-       // try {
-            for (int i = 0; i < 5; i++) {
-                System.out.println( letter );
-            }
-      //  }catch (InterruptedException e)  {
-       //     System.out.println("Поток "+ letter + " прерван");
-       //     }
-      //  }
+        synchronized (this) {
+            try {
+                while (exitNumber != counter) {
+                    this.wait();
+                }
+                for (int i = 0; i < 5; i++) {
+                    System.out.println( letter );
+                }
 
+                if (counter != maxNumber) {
+                    counter += 1;
+                } else {
+                    counter = 1;
+                }
+
+            }
+
+//        if (exitNumber == counter ) {
+//            for (int i = 0; i < 5; i++) {
+//                System.out.println( letter );
+//            }
+//
+//                if (counter!= maxNumber) {
+//                    counter += 1;
+//                } else {
+//                    counter = 1;
+//                }
+//            } else {
+//            while (exitNumber != counter) {
+//                this.wait();
+//            }
+//        }
+            //}
+         catch(InterruptedException e){
+            System.out.println( "Поток " + letter + " прерван" );
+        }
     }
+}
+
 }
