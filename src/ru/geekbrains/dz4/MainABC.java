25fc49e5c8aed7d5924ce0c8208a0dd4c0fb4dbe
AnastasiaDro <adrogunova@gmail.com>
2018-10-05 14:46:48 +0300

    Началась работа над первой частью дз №4 -Добавлен пакет dz4 -В dz4 добавлены классы MainABC и MyLetterThread

---
+++ src/ru/geekbrains/dz4/MainABC.java
@@ -0,0 +1,8 @@
+package ru.geekbrains.dz4;
+
+public class MainABC {
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/ru/geekbrains/dz4/MyLetterThread.java
@@ -0,0 +1,31 @@
+package ru.geekbrains.dz4;
+
+import org.omg.PortableServer.THREAD_POLICY_ID;
+
+public class MyLetterThread implements Runnable {
+    char letter;
+    int exitNumber;
+    Thread t;
+//конструктор
+public MyLetterThread (char letter, int exitNumber) {
+    this.letter  = letter;
+    this.exitNumber = exitNumber;
+    this.t = new Thread(  );
+    t.start();
+}
+
+
+
+    @Override
+    public void run() {
+       // try {
+            for (int i = 0; i < 5; i++) {
+                System.out.println( letter );
+            }
+      //  }catch (InterruptedException e)  {
+       //     System.out.println("Поток "+ letter + " прерван");
+       //     }
+      //  }
+
+    }
+}
