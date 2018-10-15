2e3e15921c3355732763b0801c560b02e1e998da
AnastasiaDro <adrogunova@gmail.com>
2018-10-15 10:38:16 +0300

    Гонка настроена: -все участники стартуют одновременно -в тоннель может заехать только половина участников, остальные ждут -сообщение об окончании гонки выводится только тогда, когда все участники приехали на финиш

--- src/ru/geekbrains/dz5/Tunnel.java
+++ src/ru/geekbrains/dz5/Tunnel.java
@@ -4,9 +4,13 @@ import java.util.concurrent.Semaphore;
 
 public class Tunnel extends Stage {
 
-
+//FIXME
     //благодаря Семафору в тоннель может заехать не более половины участников
-    Semaphore tonnelSemaphore = new Semaphore( MainClass.getCarsCount()/2 );
+    int tunnelCapacity = MainClass.getCarsCount()/2;
+    Semaphore TUNNEL_SEMAPHORE = new Semaphore( tunnelCapacity );
+    private final boolean[] TUNNEL_PLACES = new boolean[tunnelCapacity];
+//FIXME
+
     public Tunnel() {
         this.length = 80;
         this.description = "Тоннель " + length + " метров";
@@ -18,10 +22,33 @@ public class Tunnel extends Stage {
         try {
             try {
                 System.out.println(c.getName() + " готовится к этапу(ждет): " + description);
+ //FIXME
  //Ставим тут семафор
+                TUNNEL_SEMAPHORE.acquire();
+                int tunnelPlace = -1;
+ //ищем свободное место в тоннеле
+                synchronized (TUNNEL_PLACES) {
+                    for (int i = 0; i < TUNNEL_PLACES.length; i++) {
+                        if(!TUNNEL_PLACES[i]){
+                            TUNNEL_PLACES[i] = true;
+                            tunnelPlace = i;
+                            System.out.println(c.getName() + " начал этап: " + description);
+                            break;
+                        }
+                    }
+                }
 
-                System.out.println(c.getName() + " начал этап: " + description);
+
+
+  //              System.out.println(c.getName() + " начал этап: " + description);
                 Thread.sleep(length / c.getSpeed() * 1000);
+ //Освобождаем тоннель
+             synchronized (TUNNEL_PLACES) {
+                 TUNNEL_PLACES[tunnelPlace] = false;
+             }
+//освобождаем семафор
+            TUNNEL_SEMAPHORE.release();
+
             } catch (InterruptedException e) {
                 e.printStackTrace();
             } finally {
